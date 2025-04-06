d3d6d7083eb33e44befba37f3a3c83b8be1c1279
zhaozg <zhaozg@gmail.com>
2025-04-06 13:57:02 +0800

    fix __clear_cache build for mips with zig

--- src/lj_mcode.c
+++ src/lj_mcode.c
@@ -53,7 +53,11 @@ void lj_mcode_sync(void *start, void *end)
 #elif LJ_TARGET_PPC
   lj_vm_cachesync(start, end);
 #elif defined(__GNUC__) || defined(__clang__)
+#if defined(__mips__) || defined(__mips64)
+  UNUSED(start); UNUSED(end);
+#else
   __clear_cache(start, end);
+#endif
 #else
 #error "Missing builtin to flush instruction cache"
 #endif
